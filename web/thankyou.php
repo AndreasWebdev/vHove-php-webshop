da021e43652967d6d6ca7b5c6eb7b72f0a853b3e
Andreas Heimann <andreas@iklikla.de>
2018-09-05 12:00:37 +0200

    Create thankyou.php

---
+++ web/thankyou.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+    </head>
+    <body>
+      <?php
+        include('template/header.php');
+      ?>
+      
+      <h1>Vielen Dank!</h1>
+      
+      <?php
+        include('template/footer.php');
+      ?>
+    </body>
+  </html>
