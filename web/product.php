7c34cd909253d59a4c6273ff8af0ca6c2a1baec0
Andreas Heimann <andreas@iklikla.de>
2018-09-05 13:09:28 +0200

    Update product.php

--- web/product.php
+++ web/product.php
@@ -40,11 +40,6 @@
       <input type="number" class="dataAmount" value="1" /><br />
       <button class="addToCheckout">Zum Warenkorb hinzufügen</button>
       
-      <?php
-        setcookie('checkout', array(1 => "Test", 2 => "Warenkorb 2");
-        echo $_COOKIE['checkout'];
-      ?>
-      
       <script>
         var addToCheckoutButton = document.querySelector('.addToCheckout');
         
@@ -54,6 +49,8 @@
           var dataAmount = document.querySelector('.dataAmount').value;
           
           console.log(dataProduct + " - " + dataSize + " - " + dataAmount);
+          
+          // Save to cart
         });
       </script>
       
