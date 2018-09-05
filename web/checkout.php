1af16cc4d47cfcaa6940905a3f2f2e81cc5af418
Andreas Heimann <andreas@iklikla.de>
2018-09-05 12:00:21 +0200

    Create checkout.php

---
+++ web/checkout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+    </head>
+    <body>
+      <?php
+        include('template/header.php');
+      ?>
+      
+      <h1>Checkout</h1>
+      
+      <?php
+        include('template/footer.php');
+      ?>
+    </body>
+  </html>
