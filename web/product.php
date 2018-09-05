9af43b0a3c5ae9e335ad957477e7060c1032db1b
Andreas Heimann <andreas@iklikla.de>
2018-09-05 12:19:37 +0200

    Update product.php

--- web/product.php
+++ web/product.php
@@ -10,6 +10,36 @@
       <h1>Product</h1>
       
       <?php
+        $product = $_GET['product'];
+      
+        switch($product) {
+          case 1:
+            $productTitle = "Ruhrgebiet";
+            $productDesc = "Wir sind das Ruhrgebiet!";
+            break;
+          case 2:
+            $productTitle = "Gelsenkirchen";
+            $productDesc = "Da is Schalke04 Land und so..";
+            break;
+        }
+      ?>
+      
+      <h3><?=$productTitle;?></h3>
+      <p><?=$productDesc;?></p>
+      
+      <form action="checkout.php" method="POST">
+        <select name="size">
+          <option value="xs">XS</option>
+          <option value="s">S</option>
+          <option value="m">M</option>
+          <option value="xl">XL</option>
+          <option value="xxl">XXL</option>
+        </select><br />
+        <input type="number" name="amount" /><br />
+        <input type="submit" value="Bestellen" />
+      </form>
+      
+      <?php
         include('template/footer.php');
       ?>
     </body>
