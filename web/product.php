7a1ccc7e46ede79c2ff03d4930eb9fe87c127ec4
Andreas Heimann <andreas@iklikla.de>
2018-09-05 12:51:57 +0200

    Update product.php

--- web/product.php
+++ web/product.php
@@ -12,12 +12,10 @@
       <?php
         $product = $_GET['product'];
       
-      
         if($_POST['submit']) {
           $_COOKIE['checkout'][] = array('product' => $_POST['product'], 'size' => $_POST['size'], 'amount' => $_POST['amount']);
           echo "Erfolgreich zum Warenkorb hinzugefügt! <a href='checkout.php'>Zum Checkout</a>";
         }
-      
       
         switch($product) {
           case 1:
@@ -34,18 +32,32 @@
       <h3><?=$productTitle;?></h3>
       <p><?=$productDesc;?></p>
       
-      <form action="" method="POST">
-        <input type="hidden" name="product" value="<?=$product;?>" />
-        <select name="size">
-          <option value="xs">XS</option>
-          <option value="s">S</option>
-          <option value="m">M</option>
-          <option value="xl">XL</option>
-          <option value="xxl">XXL</option>
-        </select><br />
-        <input type="number" name="amount" value="1" /><br />
-        <input type="submit" name="submit" value="Bestellen" />
-      </form>
+      <hr />
+      
+      <input type="hidden" name="product" value="<?=$product;?>" />
+      <select name="size">
+        <option value="xs">XS</option>
+        <option value="s">S</option>
+        <option value="m">M</option>
+        <option value="xl">XL</option>
+        <option value="xxl">XXL</option>
+      </select><br />
+      <input type="number" name="amount" value="1" /><br />
+      <button class="addToCheckout">Zum Warenkorb hinzufügen</button>
+      
+      <script>
+        var addToCheckoutButton = document.querySelector('.addToCheckout');
+        
+        addToCheckoutButton.addEventListener('click', addToCheckout());
+        
+        function addToCheckout() {
+          var dataProduct = document.querySelector('input[name="product"]');
+          var dataSize = document.querySelector('input[name="size"]');
+          var dataAmount = document.querySelector('input[name="amount"]');
+          
+          console.log(dataProduct + " - " + dataSize + " - " + dataAmount);
+        }
+      </script>
       
       <?php
         include('template/footer.php');
