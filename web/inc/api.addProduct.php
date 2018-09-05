575a89adaa1a32ed755385b45ac54d4bf4295188
Andreas Heimann <andreas@iklikla.de>
2018-09-05 13:09:45 +0200

    Delete api.addProduct.php

--- web/inc/api.addProduct.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $product = $_POST['product'];
-  $size = $_POST['size'];
-  $amount = $_POST['amount'];
-  
-  $currentCheckout = $_SESSION['checkout'];
-  array_push($currentCheckout, array("product" => $product, "size" => $size, "amount" => $amount));
-  return "true";
-?>
