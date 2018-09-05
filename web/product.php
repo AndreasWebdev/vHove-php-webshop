96153e3e190714b2d407b644693d769e12e74b19
Andreas Heimann <andreas@iklikla.de>
2018-09-05 12:01:34 +0200

    Update product.php

--- web/product.php
+++ web/product.php
@@ -1,3 +1,16 @@
-<?php
-echo "ProductPage";
-?>
+<!DOCTYPE html>
+  <html>
+    <head>
+    </head>
+    <body>
+      <?php
+        include('template/header.php');
+      ?>
+      
+      <h1>Product</h1>
+      
+      <?php
+        include('template/footer.php');
+      ?>
+    </body>
+  </html>
