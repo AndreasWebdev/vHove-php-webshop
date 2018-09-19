ed4d48a4f81e52b3d93040500b2906a70e5627ba
Andreas Heimann <privat@andreas-heimann.com>
2018-09-19 12:07:11 +0200

    oops!

--- web/login.php
+++ web/login.php
@@ -11,9 +11,9 @@ if(isset($_POST['submit'])) {
 	// Get form data
 	$user = $_POST['username'];
 	$pass = $_POST['password'];
-	
+
 	// Check form data
-	if($user == "vHove" && $pass = "vHove123") {
+	if($user == "vHove" && $pass == "vHove123") {
 		// Successful, set session and redirect
 		$_SESSION['vHoveShop-login'] = true;
 		header("Location: index.php");
@@ -31,7 +31,7 @@ if(isset($_POST['submit'])) {
 		<h1>Login</h1>
 		<p>Username: vHove<br />
 			Passwort: vHove123</p>
-			
+
 		<form action="" method="post">
 			Username: <input type="text" name="username" /><br />
 			Passwort: <input type="password" name="password" /><br />
